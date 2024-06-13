2d920bab8d0a70658db0d36c2dc23d3c8142edd8
Mahmoud Abdelghany <mahmoud.a.rageh@gmail.com>
2024-06-13 20:51:24 +0300

    task2

---
+++ python/task2/task.py
@@ -0,0 +1,5 @@
+import math
+radius = input("Enter the radius of the circle: ")
+radius = float(radius)
+area = math.pi * radius ** 2
+print(f"The area of the circle with radius {radius} is {area}")

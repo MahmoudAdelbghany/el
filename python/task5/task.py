658ee09780253be202766d1da4eb87e08c2d3e96
Mahmoud Abdelghany <mahmoud.a.rageh@gmail.com>
2024-06-14 17:53:00 +0300

    task5

---
+++ python/task5/task.py
@@ -0,0 +1,10 @@
+import requests
+from pprint import pprint
+response = requests.get('https://api.ipify.org/?format=json')
+# print(response.json())
+
+ip = response.json()['ip']
+
+response = requests.get('https://ipinfo.io/'+ip+'/geo')
+
+pprint(response.json())
